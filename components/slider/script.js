060cc2388f8fff77b8192951bda254a32719fee9
Naritai <alexandr.voronetskiy@gmail.com>
2020-09-01 15:35:44 +0300

    Refactor slider component

--- components/slider/script.js
+++ components/slider/script.js
@@ -1,35 +1,35 @@
 class Slider {
 	constructor(options) {
-		this.slider = options.slider;
-		this.traveler = options.traveler;
+		this.sliderElement = options.element;
+		this.travelerElement = this.sliderElement.querySelector('.slider-line__traveler'); // looking for traveler inside component on slider
 		this.shiftX = 0;
 		this.shiftY = 0;
-		this.sliderCoords = this.slider.getBoundingClientRect();
-		this.traveler.addEventListener('dragstart', () => false);
-		this.traveler.addEventListener('mousedown', this._onMouseDown);
+		this.sliderCoords = this.sliderElement.getBoundingClientRect();
+		this.travelerElement.addEventListener('dragstart', () => false);
+		this.travelerElement.addEventListener('mousedown', this._onMouseDown);
+		this.sliderElement.addEventListener('wheel', this._onWheel);
 	};
 
 	_onMouseDown = (event) => {					// class fields syntax to save this
 		event.preventDefault(); 				// prevent text selection
-		this.traveler.style.zIndex = 1000; 		// put elem above all other elements
+		this.travelerElement.style.zIndex = 1000; 		// put elem above all other elements
 
 		if (event.target.closest('.slider-line__traveler')) { // sure that the traveler is that we really need
-			this._onStartDrag(event.clientX, event.clientY);
+			this._onStartDrag(event.clientX);
 		}
 	};
 
-	_onStartDrag = (clientX, clientY) => {
-		const travelerCoords = this.traveler.getBoundingClientRect();
+	_onStartDrag = (clientX) => {
+		const travelerCoords = this.travelerElement.getBoundingClientRect();
 		this.shiftX = clientX - travelerCoords.left;
-		this.shiftY = clientY - travelerCoords.top;
 
-		document.addEventListener('mousemove', this._onDocumentMouseMove);
-		document.addEventListener('mouseup', this._onDocumentMouseUp);
+		document.addEventListener('mousemove', this._onDocumentMouseMove); // hang handler when mouse down
+		document.addEventListener('mouseup', this._onDocumentMouseUp);	   // hang handler when mouse down
 	};
 
 	_onEndDrag = () => {
-		document.removeEventListener('mousemove', this._onDocumentMouseMove);
-		document.removeEventListener('mouseup', this._onDocumentMouseUp);
+		document.removeEventListener('mousemove', this._onDocumentMouseMove); // remove handler when mouse up
+		document.removeEventListener('mouseup', this._onDocumentMouseUp);     // remove handler when mouse up
 	}
 
 	_onDocumentMouseMove = (event) => {		
@@ -44,66 +44,15 @@ class Slider {
 		let newLeft = clientX - this.shiftX - this.sliderCoords.left;
 		newLeft = newLeft < 0 ? 0 : newLeft;
 
-		const rightEdge = this.slider.offsetWidth - this.traveler.offsetWidth;
+		const rightEdge = this.sliderElement.offsetWidth - this.travelerElement.offsetWidth;
 		newLeft = newLeft > rightEdge ? rightEdge : newLeft;
-		
-		this.traveler.style.left = newLeft + 'px';
+
+		this.travelerElement.style.left = newLeft + 'px';
 	};	
-}
-
-const slider = new Slider({
-	slider: document.querySelector('.slider-line'),
-	traveler: document.querySelector('.slider-line__traveler')
-});
+};
 
 
-// **********************************************
-// *********** SOLUTION WITHOUT CLASS ***********
-// **********************************************
-
-// traveler.addEventListener('dragstart', () => false);
-
-// traveler.addEventListener('mousedown', function(event) {
-// 	event.preventDefault(); 						// prevent text selection
-// 	traveler.style.zIndex = 1000; 					// put elem above all other elements
-
-// 	const shiftX = event.clientX - traveler.getBoundingClientRect().left;
-// 	const { left: leftBoundry } = sliderCoords;
-
-// 	moveAt(event.clientX); 							// set traveler under the cursor
-
-// 	function moveAt(clientX) {
-// 		let value = clientX - leftBoundry - shiftX;
-// 		const rightEdge = slider.offsetWidth - traveler.offsetWidth;
-
-// 		value = value < 0 ? 0 : value;
-// 		value = value > rightEdge ? rightEdge : value;
-
-// 		traveler.style.left = value + 'px';
-// 	};
-
-// 	function onMouseMove(event) {
-// 		moveAt(event.clientX);
-// 	};
-
-// 	function onMouseUp(event) {
-// 		document.removeEventListener('mousemove', onMouseMove);
-// 		document.removeEventListener('mouseup', onMouseUp);
-// 	};
-
-// 	document.addEventListener('mousemove', onMouseMove);
-// 	document.addEventListener('mouseup', onMouseUp);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
+[...document.querySelectorAll('.slider-line')]
+	.forEach(element => {
+		new Slider({ element: element})
+	});
